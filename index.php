3771320ffeeee81361d52738bc9540e1194ca66c
Mamun <abdullah.mamun@itconquest.com>
2018-07-31 15:39:28 +0600

    lttile change

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 
 <h1>CI/CD has finihsed by the grace of Almighty Allah</h1>
 <h1>CI/CD has finihsed by the grace of Almighty Allah</h1>
-<h1>CI/CD has finihsed by the grace of Almighty Allah</h1>
 
 <?php
 echo "Hello World!";
