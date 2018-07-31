48b24b5279ddcf1a442e9ff204313d5213522835
Mamun <abdullah.mamun@itconquest.com>
2018-07-31 15:49:14 +0600

    lttle change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>Yahooo! CI/CD has finihsed by the grace of Almighty Allah</h1>
+<h1>Yahoo! CI/CD has finihsed by the grace of Almighty Allah</h1>
 
 <?php
 echo "Hello World!";
