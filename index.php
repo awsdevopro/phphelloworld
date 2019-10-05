7f46d879f292ef8f9a73b127e20f3af87bb9f15b
Mamun <mamun@redltd.tech>
2019-10-05 12:44:19 +0600

    change a lil

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>CI/CD has finihsed by the grace of Almighty Allah</h1>
+<h1>CI has finihsed by the grace of Almighty Allah</h1>
 <h1>CI/CD has finihsed by the grace of Almighty Allah</h1>
 
 <?php
